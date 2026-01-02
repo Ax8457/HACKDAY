9de88d5f3267aac862b3c13003c740a409dee778
Ax8457 <147101606+Ax8457@users.noreply.github.com>
2026-01-02 18:12:24 +0100

    Update main.rs

--- Hackday2025/Finals/TheHearthOfGearford/src/main.rs
+++ Hackday2025/Finals/TheHearthOfGearford/src/main.rs
@@ -12,13 +12,13 @@ use nginx::start_server2;
 fn main() {
     // Auth server
     thread::spawn(|| {
-        println!("🚀 Auth Server running on http://127.0.0.1:8081/");
-        start_server();  // Serveur principal d'authentification
+        println!(" Auth Server running on http://127.0.0.1:8081/");
+        start_server();  
     });
 
     // API, not exposed, aims to return a json to check token before rendering php pages behind nginx reverse proxy
     thread::spawn(|| {
-        println!("🚀 Token Validation Server running on http://127.0.0.1:5000/");
+        println!(" Token Validation Server running on http://127.0.0.1:5000/");
         start_server2(); 
     });
     loop {
